946190ddae650cae4146e1fafc875048b5222455
Tanner Perry <tc-boy@hotmail.com>
2021-10-26 14:40:01 -0600

    fixed error

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const path = require('path')
+// const Rollbar = require('rollbar')
+
+// let rollbar = new Rollbar({
+//     accessToken: 'f27951a1e33a405d8dc15b3eab2a2e9a',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    // rollbar.info('html file served successfully')
+})
+// app.use(rollbar.errorHandler())
+
+const port = process.env.PORT || 4567
+
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
